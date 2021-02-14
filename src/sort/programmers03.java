f0a9a86ac8d5a93db8ffc2dbf598a9cacdfb72dd
sehyeon <an143028@yonsei.ac.kr>
2021-02-15 00:01:23 +0900

    [programmers] coding test exercise sort2,3

---
+++ src/sort/programmers03.java
@@ -0,0 +1,18 @@
+package sort;
+
+import java.util.Arrays;
+
+public class programmers03 {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+        return answer;
+    }
+}
